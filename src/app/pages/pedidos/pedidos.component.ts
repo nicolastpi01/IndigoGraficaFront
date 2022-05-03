524f2f171c5dc201c87bff39fd5fa0fa1fe8b2c2
nicolastpi01 <nicolastpi10@gmail.com>
2022-05-03 19:36:19 -0300

    corregido error build

--- src/app/pages/pedidos/pedidos.component.ts
+++ src/app/pages/pedidos/pedidos.component.ts
@@ -23,7 +23,7 @@ export class PedidosComponent implements OnInit {
   }
 
   getPedidos(): void {
-    this.servicefdsfsdfsd f.getPedidos(PENDIENTEATENCION)
+    this.service.getPedidos(PENDIENTEATENCION)
     .subscribe(pedidos => this.pedidos = pedidos);
   }
 
