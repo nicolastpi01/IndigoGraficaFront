d14aa61a95732790a4b20930732f82fc67687bcd
nicolastpi01 <nicolastpi10@gmail.com>
2022-05-16 20:32:22 -0300

    fix. errores de compilacion

--- src/app/interface/pedido.ts
+++ src/app/interface/pedido.ts
@@ -5,33 +5,35 @@ import { Usuario } from "./usuario";
 
 export class Pedido {
     id?: number;
-    propietario: Usuario;
-    cantidad: number; 
-    nombre: string; 
-    nombreExtendido: string; 
+    propietario!: Usuario;
+    cantidad!: number; 
+    nombre!: string; 
+    nombreExtendido!: string; 
     tipografia?: string; 
     tipo?: Tipo; 
     alto?: number; 
     ancho?: number; 
-    descripcion: string; 
-    colores: string[]; 
+    descripcion?: string; 
+    colores!: string[]; 
     boceto?: File;
-    estado: Estado;
+    estado!: Estado;
     editor?: Usuario;
+
+    Pedido() {}
     
     errores: Error[] = [];
 
     validar(): Error[] {
-        errores = [];
+        let errores = [];
         if (!this.propietario) {
             errores.push({ campo: 'propietario', mensajeValidacion: 'Debe ingresar propietario' });
         }
         return errores;
     }  
     
-    errorParaElCampo(campo: String): String | undefined {
-        this.errores.find((_error) => _error.campo = campo);
-    }
+    errorParaElCampo = (campo: String): Error | undefined => {
+        return this.errores.find((_error) => _error.campo = campo);
+    };
 }
 
 export interface Error {
