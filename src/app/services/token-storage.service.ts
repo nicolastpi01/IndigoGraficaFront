1de8b29d97953a8a17e4bcc5f3a52aeaa5988898
Diego54 <d.caminos54@gmail.com>
2022-06-08 21:35:20 -0300

    Se agregan restricciones sobre las secciones cuando no hay usuario loggeado

--- src/app/services/token-storage.service.ts
+++ src/app/services/token-storage.service.ts
@@ -30,6 +30,6 @@ export class TokenStorageService {
   }
 
   public getUser() {
-    return JSON.parse(sessionStorage.getItem(USER_KEY)||'');
+    return JSON.parse(sessionStorage.getItem(USER_KEY)||'{}');
   }
 }
