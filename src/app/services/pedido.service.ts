6663ba6e1caccbfb10b8323e4136fcfb21862bcc
nicolas <nicolastpi10@gmail.com>
2022-11-12 07:36:07 -0300

    update

--- src/app/services/pedido.service.ts
+++ src/app/services/pedido.service.ts
@@ -94,4 +94,13 @@ export class PedidoService {
     return this.http.put(`${this.baseUrl}/pedidos/` + pedido.id, this.httpOptions).pipe()
   }
 
+  // Voy a buscar el Pedido con el id, si el Pedido esta Reservado (y no esta Reservado por mi, ergo soy el Editor) entonces
+  // lanzo una excepcion 'No puede Editar un Pedido que ya fue reservado --y agrego una Sol. alternativa'
+  // Si el Pedido no esta Reservado o esta Reservado pero por mi entonces le permito Editar, y retorno el Pedido con un Estado nuevo
+  // Estado = available = False; - De este modo un Editor no puede 'Reservarlo' si quisiera
+  allowsEdit(idPedido: string | null) :Observable<Pedido> {
+    //const req = new HttpRequest('POST', `${this.baseUrl}`+this.api+'/create', httpOptions);
+    return this.http.get<Pedido>(`${this.baseUrl}/pedidos/AllowsEdit/` + idPedido, httpOptions);
+  }
+
 }
