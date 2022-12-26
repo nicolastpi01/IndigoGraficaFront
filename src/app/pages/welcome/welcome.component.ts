0ad3c21de0d73f2c548f23b4bbe39f9fceaedd27
Diego54 <d.caminos54@gmail.com>
2022-12-26 12:34:58 -0300

    Mejoras en pantalla bienvenida (#14)
    * Validaciones presupuesto
    
    * Se centra el nombre de usuario en el header y se acomodan el css
    
    * fix responsive header
    
    * Se dejan iguales las tarjetitas de files de Resolver y Carrito
    
    * Fix Nuevo test
    
    * mejoras en la pantalla de bienvenida

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -10,11 +10,13 @@ export class WelcomeComponent implements OnInit {
 
   usuario=''
   mensajeBienvenida= 'Bienvenido! Registrate o ingresá para hacer tus pedidos'
+  isLoggedIn = false;
 
   constructor(private tokenStorage: TokenStorageService,) { }
 
   ngOnInit() {
     const usuarioLogeado = this.tokenStorage.getUser()
+    this.isLoggedIn = !!this.tokenStorage.getToken();
     this.usuario = usuarioLogeado.username
     if(this.usuario){
       this.mensajeBienvenida=`Bienvenido ${this.usuario}.`
