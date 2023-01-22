608f0314aa0dc3f2bd575cec1a31a8c2f84cfd12
nicolas <nicolastpi10@gmail.com>
2023-01-22 04:17:10 -0300

    agrego casos de test en app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,38 +1,59 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  //let comp: AppComponent;
+  //let fixture: ComponentFixture<AppComponent>;
+  //let de: DebugElement;
+  //let el: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ],
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
-  });
-/*
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    }).compileComponents().then(() => {
+      //fixture = TestBed.createComponent(AppComponent);
+      
+      //comp = fixture.componentInstance; //AppComponent text instance
+
+      //de = fixture.debugElement.query(By.css('form'));
+
+      //el = de.nativeElement;
+    });
   });
 
-  it(`should have as title 'indigo'`, () => {
+  it("Debe tener titulo 'indigo' ", () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('indigo');
   });
-/*
-  it('should render title', () => {
+
+  it("Debe renderizar 'Índigo Gráfica' en el tag h1", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('indigo app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Índigo Gráfica');
   });
-  */
+
+  it("Está logueado entonces no debe renderizar el botón de 'Ingresar' ", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const component = fixture.componentInstance;
+    
+    component.isLoggedIn = true;
+    fixture.detectChanges()
+    const compiled = fixture.debugElement.query(By.css('[data-testid="ingresar"]'));
+    expect(compiled).toBeNull();
+  });
 
 });
 
